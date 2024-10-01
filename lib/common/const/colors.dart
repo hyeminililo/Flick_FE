36f3b472389d39d6d7dd7473ffa85fd4ecac49f2
Lee Seungchang <jjekrrq01@naver.com>
2024-10-02 00:41:48 +0900

    #8 #10 feat: 뉴스 상세 페이지 및 홈화면 UI구현

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+const PRIMARY_COLOR = Color(0xff47D294);
+
+const STUDENT_BUTTON_COLOR = Color(0xffff6c3e);
+
+const MAIN_TEXT_COLOR = Color(0x00000000);
+
+const SUB_COLOR = Color(0xffD5D7E3);
+
+const MAIN_BG_COLOR = Color(0xffffffff);
