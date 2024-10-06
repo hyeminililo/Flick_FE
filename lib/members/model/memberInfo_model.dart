df730b17ba3a767f4c3221d62ee373694f5cac7e
hyeminililo <ng040115@gmail.com>
2024-10-06 15:11:38 +0900

    #18 Feat : 멤버 정보 받아오는 model 구현
    온보딩 때 입력한 정보를 받아오는 model 구현

--- lib/members/model/memberInfo_model.dart
+++ lib/members/model/memberInfo_model.dart
@@ -1,1 +1,37 @@
-class MemberInfoModel {}
+import 'package:flick_frontend/members/model/members_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'memberInfo_model.g.dart';
+
+@JsonSerializable()
+class MemberInfoModel {
+  final int id;
+  final String email;
+  final String? picture;
+  final String nickname;
+  final String? school;
+  final String? gradeClass;
+  final UserType type;
+  final int? score;
+  final int? ecoLifeDuration;
+  final String? recentChallengeTitle;
+  final int? totalActionCount;
+
+  MemberInfoModel(
+      {required this.id,
+      required this.email,
+      this.picture,
+      required this.nickname,
+      this.school,
+      this.gradeClass,
+      required this.type,
+      this.score,
+      this.ecoLifeDuration,
+      this.recentChallengeTitle,
+      this.totalActionCount});
+
+  factory MemberInfoModel.fromJson(Map<String, dynamic> json) =>
+      _$MemberInfoModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MemberInfoModelToJson(this);
+}
