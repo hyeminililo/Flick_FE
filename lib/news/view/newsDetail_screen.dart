3c301e39d7ea9a042f53705223e31e5228b15f6f
hyeminililo <ng040115@gmail.com>
2024-11-24 13:07:07 +0900

    #23 style : 환경 뉴스 UI 수정

--- lib/news/view/newsDetail_screen.dart
+++ lib/news/view/newsDetail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flick_frontend/common/const/colors.dart';
 import 'package:flick_frontend/news/provider/newsDetails_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,66 +16,123 @@ class NewsDetailScreen extends ConsumerWidget {
     final screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('뉴스 상세보기'),
-      ),
       body: newsAsyncValue.when(
         data: (news) {
           if (news == null) {
             return const Center(child: Text('뉴스를 찾을 수 없습니다.'));
           }
 
-          return SingleChildScrollView(
-            padding: EdgeInsets.all(screenSize.width * 0.04), // 동적으로 패딩 설정
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (news.picture != null)
-                  Image.network(
-                    news.picture!,
-                    width: double.infinity,
-                    height: screenSize.height * 0.25, // 동적으로 높이 설정
-                    fit: BoxFit.cover,
-                  )
-                else
-                  Container(
-                    width: double.infinity,
-                    height: screenSize.height * 0.25, // 동적으로 높이 설정
-                    color: Colors.grey,
-                    child: const Center(child: Text('이미지가 없습니다.')),
-                  ),
-                const SizedBox(height: 16),
-                Text(
-                  news.title,
-                  style: TextStyle(
-                    fontSize: screenSize.width * 0.06, // 동적으로 폰트 크기 설정
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  news.contents,
-                  style: TextStyle(
-                      fontSize: screenSize.width * 0.045), // 동적으로 폰트 크기 설정
-                ),
-                const SizedBox(height: 16),
-                Row(
+          return Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Icon(Icons.visibility),
-                    const SizedBox(width: 5),
-                    Text('${news.viewCount} views'),
+                    if (news.picture != null)
+                      Image.network(
+                        news.picture!,
+                        width: double.infinity,
+                        height: screenSize.height * 0.4, // 동적으로 높이 설정
+                        fit: BoxFit.fill,
+                      )
+                    else
+                      Container(
+                        width: double.infinity,
+                        height: screenSize.height * 0.4, // 동적으로 높이 설정
+                        color: Colors.grey,
+                        child: const Center(child: Text('이미지가 없습니다.')),
+                      ),
+                    Padding(
+                      padding: EdgeInsets.all(screenSize.width * 0.04), // 동적 패딩
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 16),
+                          Text(
+                            news.title,
+                            style: TextStyle(
+                              fontSize:
+                                  screenSize.width * 0.06, // 동적으로 폰트 크기 설정
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: screenSize.height * 0.005, // 동적 세로 패딩
+                              horizontal: screenSize.width * 0.03, // 동적 가로 패딩
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.green.shade400, // 배경색
+                              borderRadius: BorderRadius.circular(15), // 둥근 모서리
+                            ),
+                            child: Text(
+                              '환경뉴스',
+                              style: TextStyle(
+                                fontSize: screenSize.width * 0.04, // 동적 텍스트 크기
+                                fontWeight: FontWeight.bold, // 텍스트를 굵게
+                                color: Colors.white, // 텍스트 색상
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text(
+                                " ${news.broadcastingCompany} | ${news.date}",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  color: const Color.fromARGB(255, 107, 108,
+                                      113), // 텍스트 색상 screenSize.width * 0.04, // 동적 텍스트 크기
+                                ),
+                              ),
+                              SizedBox(
+                                width: screenSize.width * 0.37,
+                              ),
+                              const Icon(Icons.visibility),
+                              SizedBox(width: 5),
+                              Text(
+                                '${news.viewCount} views',
+                                textAlign: TextAlign.end,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            news.contents,
+                            style: TextStyle(
+                                fontSize:
+                                    screenSize.width * 0.045), // 동적으로 폰트 크기 설정
+                          ),
+                          const SizedBox(height: 16),
+
+                          // Row(
+                          //   children: [
+                          //     const Icon(Icons.thumb_up),
+                          //     const SizedBox(width: 5),
+                          //     Text('${news.likeCount} likes'),
+                          //   ],
+                          // ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                const SizedBox(height: 16),
-                Row(
-                  children: [
-                    const Icon(Icons.thumb_up),
-                    const SizedBox(width: 5),
-                    Text('${news.likeCount} likes'),
-                  ],
+              ),
+              // 뒤로가기 버튼
+              Positioned(
+                top: 5,
+                left: 6,
+                child: SafeArea(
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // 뒤로가기
+                    },
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           );
         },
         loading: () => const Center(child: CircularProgressIndicator()),
