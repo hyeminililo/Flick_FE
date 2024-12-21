d23f49873547c5e0fc7c0573b3af3a75516f6ce4
hyeminililo <ng040115@gmail.com>
2024-12-22 05:08:08 +0900

    #36 build : 패키지 이름 수정 및 배포 파일 생성

--- android/app/src/main/kotlin/com/example/flick_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flick_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/onef/flick/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.onef.flick
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
